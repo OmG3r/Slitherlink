a91d19b91eb3888d7f8b4e4cab127378f73b7402
omg3r <adam.boulila@live.fr>
2021-06-02 20:26:18 +0100

    up

--- click.py
+++ click.py
@@ -5,6 +5,7 @@ import math
 view = ""
 elements = []
 clickables = {}
+segmentsRef = {}
 gameTexts = {}
 def createButton(x = 0, y = 0, width=150, height=50, content="", fontSize = 12):
     global elements
@@ -80,50 +81,136 @@ def lireJeu(filename = ""):
 
 def createBox():
     pass
+def verifyTuplesOrder(segment):
+    tup1 = segment[0]
+    tup2 = segment[1]
 
-def handleGameClick(coords):
-    global allowableXs
-    global allowableYs
+    if tup1[0] > tup2[0]:
+        return (tup2, tup1)
+
+    if tup1[0] == tup2[0] and tup1[1] > tup2[1]:
+        return (tup2, tup1)
+
+    return segment
+
+def handleGameClick(mouse, coords):
+    global etat
+    global indices
     squareSide = 20
     distanceBetweenSquares = 60
     padding = 40
     x, y = coords
     print("for :" + str(coords))
-    allowedX = False
-    allowedY = False
-    i = -1
-    j = -1
-    for index ,[lower, upper] in enumerate(allowableXs):
-        
-        if lower <= x and x <= upper:
-            print("X: " + str([lower, upper]))
-            allowedX = True
-            j = index
-            break
-    for index, [lower, upper] in enumerate(allowableYs):
-        if lower <= y and y <= upper:
-            print("y: " + str([lower, upper]))
-            i = index
-            allowedY = True
-            break
-    if allowedX :
-        i = math.ceil((y - padding) / (squareSide + distanceBetweenSquares))
-        print("clicked a vertical line")
-        print("line num: " + str(i))
 
-    if allowedY:
-        j = math.ceil((x - padding) / (squareSide + distanceBetweenSquares))
-        print("clicked horizonal line")
-        print("line num: " + str(j))
+    i = math.ceil((y - padding) / (squareSide + distanceBetweenSquares)) - 1
+    j = math.ceil((x - padding) / (squareSide + distanceBetweenSquares)) - 1
+
+    row = len(indices)
+    column = len(indices[0])
+    print(row)
+    print(column)
+    if i > row  or i < 0:
+        return
+    if j > column  or j < 0:
+        return
+    sommetY = i * (squareSide + distanceBetweenSquares) + padding
+    sommetX = j * (squareSide + distanceBetweenSquares) + padding
+    print("sommet: " + str((i, j)))
+    print("sommet coords: " + str((sommetX, sommetY)))
+
+    horizontalRectangle = {
+        'x': {
+            'min': sommetX + squareSide,
+            'max': sommetX + squareSide + distanceBetweenSquares
+        },
+        'y': {
+            'min': sommetY,
+            'max': sommetY + squareSide
+        }
+    }
+
+    verticalRectangle = {
+        'x': {
+            'min': sommetX,
+            'max': sommetX + squareSide
+        },
+        'y': {
+            'min': sommetY + squareSide,
+            'max': sommetY + squareSide + distanceBetweenSquares
+        }
+    }
+
+    xInHorizontalRectangle = horizontalRectangle['x']['min'] <= x and x <= horizontalRectangle['x']['max']
+    yInHorizontalRectangle = horizontalRectangle['y']['min'] <= y and y <= horizontalRectangle['y']['max']
+
+
+    xInVerticalRectangle = verticalRectangle['x']['min'] <= x and x <= verticalRectangle['x']['max']
+    yInVerticalRectangle = verticalRectangle['y']['min'] <= y and y <= verticalRectangle['y']['max']
     
 
-allowableXs = []
-allowableYs = []
+    isHorizontal = xInHorizontalRectangle and yInHorizontalRectangle
+    isVertical = xInVerticalRectangle and yInVerticalRectangle
+    if xInHorizontalRectangle and yInHorizontalRectangle:
+        segment = ((i, j), (i, j+1))
+        rectangle(horizontalRectangle['x']['min'], horizontalRectangle['y']['min'] + 4, horizontalRectangle['x']['max'], horizontalRectangle['y']['max'] - 4, remplissage="black")
+        print(segment)
+    if xInVerticalRectangle and yInVerticalRectangle:
+        segment = ((i, j), (i + 1, j))
+        verdict = etat.get(segment, False)
+        print(verdict)
+        if verdict == False:
+            
+            if mouse == "ClicGauche":
+                etat[segment] = 1
+                segmentsRef[segment] =  rectangle(verticalRectangle['x']['min'] + 4, verticalRectangle['y']['min'], verticalRectangle['x']['max'] - 4, verticalRectangle['y']['max'], remplissage="black")
+            elif mouse == "ClicDroit":
+                etat[segment] = -1
+                centerX = (verticalRectangle['x']['min'] + verticalRectangle['x']['max']) / 2
+                centerY = (verticalRectangle['y']['min'] + verticalRectangle['y']['max']) / 2
+                segmentsRef[segment] = rectangle(centerX - 5 , centerY - 5, centerX + 5, centerY + 5, remplissage="red")
+
+        elif verdict == 1:
+            if mouse == "ClicGauche":
+                efface(segmentsRef[segment])
+                segmentsRef.pop(segment, None)
+                etat.pop(segment, None)
+                etat[segment] = -1
+                centerX = (verticalRectangle['x']['min'] + verticalRectangle['x']['max']) / 2
+                centerY = (verticalRectangle['y']['min'] + verticalRectangle['y']['max']) / 2
+                segmentsRef[segment] = rectangle(centerX - 5 , centerY - 5, centerX + 5, centerY + 5, remplissage="red")
+
+                pass
+            elif mouse == "ClicDroit":
+                efface(segmentsRef[segment])
+                segmentsRef.pop(segment, None)
+                etat.pop(segment, None)
+                etat[segment] = -1
+                centerX = verticalRectangle['x']['min'] + verticalRectangle['x']['max']
+                centerY = verticalRectangle['y']['min'] + verticalRectangle['y']['max']
+                segmentsRef[segment] = rectangle(centerX - 5 , centerY - 5, centerX + 5, centerY + 5, remplissage="red")
+                pass
+            
+            print("exist must delete")
+
+        elif verdict == -1:
+            if mouse == "ClicGauche":
+                efface(segmentsRef[segment])
+                etat[segment] = 1
+                segmentsRef[segment] =  rectangle(verticalRectangle['x']['min'] + 4, verticalRectangle['y']['min'], verticalRectangle['x']['max'] - 4, verticalRectangle['y']['max'], remplissage="black")
+                
+                pass
+            elif mouse == "ClicDroit":
+                efface(segmentsRef[segment])
+                segmentsRef.pop(segment, None)
+                etat.pop(segment, None)
+                pass
+        print("in vertical")
+        print(segment)
+    
 def drawGrid():
     global indices
     global gameTexts
-    global allowableXs
-    global allowableYs
+
     row = len(indices)
     column = len(indices[0])
     squareSide = 20
@@ -135,13 +222,13 @@ def drawGrid():
     for j in range(0, column + 1):
         starty = padding + j * (squareSide + distanceBetweenSquares)
         endy = padding + j * (squareSide + distanceBetweenSquares) + squareSide
-        allowableYs.append([starty, endy])
+
         for i in range(0, row + 1):
             
             color = "black" if i % 2 == 0 else  "green"
             startx = padding + i * (squareSide + distanceBetweenSquares)
             endx = padding + i * (squareSide + distanceBetweenSquares)+ squareSide
-            allowableXs.append((startx, endx))
+
             print({"x": {"min": startx, "max": endx}, "y": {"min": starty, "max": endy}})
             rectangle(startx, starty, endx, endy, remplissage=color)
             
@@ -150,9 +237,7 @@ def drawGrid():
                 lstarty = padding + j * (squareSide + distanceBetweenSquares) + squareSide + distanceBetweenSquares / 2
                 gameTexts[(j, i)] = texte(lstartx, lstarty,str(indices[j][i]), ancrage='center', taille=14)
     print(gameTexts)
-    allowableXs = [ list(allowable) for allowable in list(set(allowableXs))]
-    print(allowableXs)
-    print(allowableYs)
+
     #rectangle(200,100,300,150)
     #rectangle(300,400 ,300 ,400)
     mise_a_jour()
@@ -198,12 +283,13 @@ while True:
 
     elif tev == "ClicDroit":
         print("Clic droit au point", (abscisse(ev), ordonnee(ev)))
-
+        if view == "game":
+            handleGameClick(tev, (abscisse(ev), ordonnee(ev)))
     elif tev == "ClicGauche":
         if view in ['main', "grilleSelection"]:
             handleClick((abscisse(ev), ordonnee(ev)))
         elif view == "game":
-            handleGameClick((abscisse(ev), ordonnee(ev)))
+            handleGameClick(tev, (abscisse(ev), ordonnee(ev)))
         print("Clic gauche au point", (abscisse(ev), ordonnee(ev)))
 
     elif tev == 'Quitte':  # on sort de la boucle
