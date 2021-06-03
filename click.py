10c5c4e97864d6ae31334b9579ac2b41b58848f4
omg3r <adam.boulila@live.fr>
2021-06-03 19:57:49 +0200

    up

--- click.py
+++ click.py
@@ -7,6 +7,10 @@ elements = []
 clickables = {}
 segmentsRef = {}
 gameTexts = {}
+
+squareSide = 20
+distanceBetweenSquares = 60
+padding = 40
 def createButton(x = 0, y = 0, width=150, height=50, content="", fontSize = 12):
     global elements
     a = rectangle(x, y, x + width, y + height)
@@ -93,12 +97,72 @@ def verifyTuplesOrder(segment):
 
     return segment
 
+
+
+def statut_case(indices, etat, case):
+    i, j = case
+    indice = indices[i][j]
+    if indice == None:
+        return None
+    pts = [
+        (i,j),
+        (i, j + 1),
+        (i + 1, j + 1),
+        (i + 1 , j)
+    ]
+    segments = [
+        (pts[0], pts[1]),
+        (pts[1], pts[2]),
+        (pts[2], pts[3]),
+        (pts[3], pts[0])
+    ]
+    segments = [verifyTuplesOrder(segment) for segment in segments]
+    
+    nber_trace = 0
+    nber_veirge = 0
+    nber_interdit = 0
+    for segment in segments:
+        nber_trace = nber_trace + int(est_trace(etat, segment))
+        nber_veirge = nber_veirge + int(est_vierge(etat, segment))
+        nber_interdit = nber_interdit + int(est_interdit(etat, segment))
+    pass
+
+    if nber_trace== indice:
+        return 0
+
+    if (nber_trace < indice) and (nber_trace + nber_interdit < 4) and (nber_veirge > 0):
+        return 1
+
+    if (nber_trace > indice) or (nber_veirge < indice - nber_trace):
+        return -1
+
+
+def est_trace(etat, segment):
+    segment = verifyTuplesOrder(segment)
+    if etat.get(segment, None) == 1:
+        return True
+    return False
+
+def est_interdit(etat, segment):
+    segment = verifyTuplesOrder(segment)
+    if etat.get(segment, None) == -1:
+        return True
+    return False
+
+
+def est_vierge(etat, segment):
+    segment = verifyTuplesOrder(segment)
+    if etat.get(segment, None) == None:
+        return True
+    return False
+
+
 def handleGameClick(mouse, coords):
     global etat
     global indices
-    squareSide = 20
-    distanceBetweenSquares = 60
-    padding = 40
+    global squareSide
+    global distanceBetweenSquares
+    global padding
     x, y = coords
     print("for :" + str(coords))
 
@@ -113,6 +177,7 @@ def handleGameClick(mouse, coords):
         return
     if j > column  or j < 0:
         return
+    sommet = (i, j)
     sommetY = i * (squareSide + distanceBetweenSquares) + padding
     sommetX = j * (squareSide + distanceBetweenSquares) + padding
     print("sommet: " + str((i, j)))
@@ -150,92 +215,265 @@ def handleGameClick(mouse, coords):
 
     isHorizontal = xInHorizontalRectangle and yInHorizontalRectangle
     isVertical = xInVerticalRectangle and yInVerticalRectangle
+
+    rect = {}
+    segment = ()
+    direction = ""
     if xInHorizontalRectangle and yInHorizontalRectangle:
         segment = ((i, j), (i, j+1))
-        rectangle(horizontalRectangle['x']['min'], horizontalRectangle['y']['min'] + 4, horizontalRectangle['x']['max'], horizontalRectangle['y']['max'] - 4, remplissage="black")
+        rect = horizontalRectangle
+        direction = "horizontal"
+        #rectangle(horizontalRectangle['x']['min'], horizontalRectangle['y']['min'] + 4, horizontalRectangle['x']['max'], horizontalRectangle['y']['max'] - 4, remplissage="black")
         print(segment)
-    if xInVerticalRectangle and yInVerticalRectangle:
+    elif xInVerticalRectangle and yInVerticalRectangle:
         segment = ((i, j), (i + 1, j))
-        verdict = etat.get(segment, False)
-        print(verdict)
-        if verdict == False:
+        rect = verticalRectangle
+        direction = "vertical"
+    else:
+        return
+
+    def createRect(rect, direction, padding = 4):
+        if direction == "vertical":
+            return rectangle(rect['x']['min'] + padding, rect['y']['min'], rect['x']['max'] - padding, rect['y']['max'], remplissage="black")
+        elif direction == "horizontal":
+            return rectangle(rect['x']['min'], rect['y']['min'] + padding, rect['x']['max'], rect['y']['max'] - padding, remplissage="black")
+        pass
+
+    verdict = etat.get(segment, False)
+    print(verdict)
+    if verdict == False:
+        
+        if mouse == "ClicGauche":
+            etat[segment] = 1
+            segmentsRef[segment] =  createRect(rect, direction)
+        elif mouse == "ClicDroit":
+            etat[segment] = -1
+            centerX = (rect['x']['min'] + rect['x']['max']) / 2
+            centerY = (rect['y']['min'] + rect['y']['max']) / 2
+            segmentsRef[segment] = rectangle(centerX - 5 , centerY - 5, centerX + 5, centerY + 5, remplissage="red")
+
+    elif verdict == 1:
+        if mouse == "ClicGauche":
+            print("verdict = 1, click = gauche")
+            efface(segmentsRef[segment])
+            segmentsRef.pop(segment, None)
+            etat.pop(segment, None)
             
-            if mouse == "ClicGauche":
-                etat[segment] = 1
-                segmentsRef[segment] =  rectangle(verticalRectangle['x']['min'] + 4, verticalRectangle['y']['min'], verticalRectangle['x']['max'] - 4, verticalRectangle['y']['max'], remplissage="black")
-            elif mouse == "ClicDroit":
-                etat[segment] = -1
-                centerX = (verticalRectangle['x']['min'] + verticalRectangle['x']['max']) / 2
-                centerY = (verticalRectangle['y']['min'] + verticalRectangle['y']['max']) / 2
-                segmentsRef[segment] = rectangle(centerX - 5 , centerY - 5, centerX + 5, centerY + 5, remplissage="red")
-
-        elif verdict == 1:
-            if mouse == "ClicGauche":
-                efface(segmentsRef[segment])
-                segmentsRef.pop(segment, None)
-                etat.pop(segment, None)
-                etat[segment] = -1
-                centerX = (verticalRectangle['x']['min'] + verticalRectangle['x']['max']) / 2
-                centerY = (verticalRectangle['y']['min'] + verticalRectangle['y']['max']) / 2
-                segmentsRef[segment] = rectangle(centerX - 5 , centerY - 5, centerX + 5, centerY + 5, remplissage="red")
-
-                pass
-            elif mouse == "ClicDroit":
-                efface(segmentsRef[segment])
-                segmentsRef.pop(segment, None)
-                etat.pop(segment, None)
-                etat[segment] = -1
-                centerX = verticalRectangle['x']['min'] + verticalRectangle['x']['max']
-                centerY = verticalRectangle['y']['min'] + verticalRectangle['y']['max']
-                segmentsRef[segment] = rectangle(centerX - 5 , centerY - 5, centerX + 5, centerY + 5, remplissage="red")
-                pass
             
-            print("exist must delete")
 
-        elif verdict == -1:
-            if mouse == "ClicGauche":
-                efface(segmentsRef[segment])
-                etat[segment] = 1
-                segmentsRef[segment] =  rectangle(verticalRectangle['x']['min'] + 4, verticalRectangle['y']['min'], verticalRectangle['x']['max'] - 4, verticalRectangle['y']['max'], remplissage="black")
-                
-                pass
-            elif mouse == "ClicDroit":
-                efface(segmentsRef[segment])
-                segmentsRef.pop(segment, None)
-                etat.pop(segment, None)
-                pass
+            pass
+        elif mouse == "ClicDroit":
+            print("verdict = 1, click = droie")
+            efface(segmentsRef[segment])
+            segmentsRef.pop(segment, None)
+            etat.pop(segment, None)
+            etat[segment] = -1
+            centerX = (rect['x']['min'] + rect['x']['max']) / 2
+            centerY = (rect['y']['min'] + rect['y']['max']) / 2
+            print((centerX, centerY))
+            segmentsRef[segment] = rectangle(centerX - 5 , centerY - 5, centerX + 5, centerY + 5, remplissage="red")
+            pass
+        
+        print("exist must delete")
+
+    elif verdict == -1:
+        if mouse == "ClicGauche":
+            efface(segmentsRef[segment])
+            etat[segment] = 1
+            segmentsRef[segment] =  createRect(rect, direction)
+            
+            pass
+        elif mouse == "ClicDroit":
+            efface(segmentsRef[segment])
+            segmentsRef.pop(segment, None)
+            etat.pop(segment, None)
+            pass
         print("in vertical")
         print(segment)
+
+    verifyCases = []
+    maxRow = len(indices)
+    maxColumn = len(indices[0])
+    if direction == "vertical":
+        if j == 0:
+            verifyCases = [(i, j)]
+            pass
+        elif j == maxColumn:
+            verifyCases = [(i, j - 1)]
+            pass
+        else:
+            verifyCases = [(i, j), (i, j - 1)]
+            pass 
+
+    elif direction == "horizontal":
+        if i == 0:
+            verifyCases = [(i,j)]
+        elif i == maxRow:
+            verifyCases = [(i - 1, j)]
+            pass
+        else:
+            verifyCases = [(i, j), (i - 1, j)]
+            pass
+    
+    print("verifyCases : " + str(verifyCases))
+
+    def updateTexte(case, color):
+        i , j = case
+        global gameTexts
+        global indices
+        global squareSide
+        global distanceBetweenSquares
+        global padding
+        indice = indices[i][j]
+        efface(gameTexts[case])
+        lstartx = padding + j * (squareSide + distanceBetweenSquares) + squareSide + distanceBetweenSquares / 2
+        lstarty = padding + i * (squareSide + distanceBetweenSquares) + squareSide + distanceBetweenSquares / 2
+        gameTexts[case] = texte(lstartx, lstarty,str(indice), ancrage='center', taille=14, couleur=color)
+        pass
+
+    for case in verifyCases:
+        res = statut_case(indices, etat, case)
+        print("for case : " + str(case) + " result : " + str(res))
+        if res == None:
+            pass
+        if res == 0:
+            updateTexte(case, 'green')
+        elif res == 1:
+            updateTexte(case, 'black')
+        elif res == -1:
+            updateTexte(case, 'red')
+
+    VerifyLockedPremises()
+    verifyAllCasesGreen()
+
+    if VerifyLockedPremises() and verifyAllCasesGreen():
+        print("you have won")
+    else:
+        print("no win yet")
+
+
+def getSommetSegments(sommet):
+        i, j = sommet
+        global indices
+
+        maxRow = len(indices)
+        maxColumn = len(indices[0])
+
+        candidates = [
+            ((i, j), (i, j + 1)),
+            ((i, j), (i + 1, j)),
+            ((i,j), (i, j - 1)),
+            ((i,j), (i - 1, j))
+        ]
+        if i == 0:
+            candidates.remove(((i,j), (i - 1, j)))
+        if i == maxRow:
+            candidates.remove(((i, j), (i + 1, j)))
+        
+        if j == 0:
+            candidates.remove(((i,j), (i, j - 1)))
+
+        if j == maxColumn:
+            candidates.remove(((i, j), (i, j + 1)))
+        candidates = [verifyTuplesOrder(candidate) for candidate in candidates]
+        return candidates
+            
+def VerifyLockedPremises():
+    global etat
+    global indices
+
+
+    allSegments = [segment for segment,status in etat.items() if status == 1]
+    if len(allSegments) == 0:
+        print("no lines")
+        return False
+    firstSegment = allSegments[0]
+    firstSommet = firstSegment[0]
+    res = thatFunc(previousSommet=firstSommet, segment=firstSegment, verifiedsegments=[], allSegments=allSegments)
+    print("res for verifylocked :" + str(res))
+    return res
+
+
+def verifyAllCasesGreen():
+    global indices
+    global etat
+    for i, val in enumerate(indices):
+        for j, case in enumerate(val):
+            if case == None:
+                continue
+            if statut_case(indices, etat, (i, j)) != 0:
+                print("case :" + str((i, j)) + " is not green")
+                return False
+    print("all cases green")
+    return True
+def thatFunc(previousSommet=(), segment=((), ()), verifiedsegments=[], allSegments=[]):
+    if segment in verifiedsegments:
+        print("lngth verfiedsegments = " + str(len(verifiedsegments)) + " all segments length = " + str(len(allSegments)))
+        if len(verifiedsegments) != len(allSegments):
+            print("lose end not included in premises")
+            return False
+        
+        return True
+
+    segment = verifyTuplesOrder(segment)
+    segmentList = list(segment)
+    segmentList.remove(previousSommet)
+    nextSommet = segmentList[0]
+    possibleSegments = getSommetSegments(nextSommet)
+
+    possibleSegments.remove(segment)
+    candidateSegments = possibleSegments
+    matchs = []
+    for candidate in candidateSegments:
+        res = etat.get(candidate, False)
+        if res == 1:
+            matchs.append(candidate)
+
+    if len(matchs) == 0:
+        print("premises not locked at " + str(nextSommet))
+        return False
+    elif len(matchs) > 1:
+        print("premises have loss ends")
+        return False
+    elif len(matchs) == 1:
+        print("perfect going for next segment")
+        verifiedsegments.append(segment)
+        return thatFunc(previousSommet=nextSommet, segment=matchs[0], verifiedsegments=verifiedsegments, allSegments=allSegments)
+
     
 def drawGrid():
     global indices
     global gameTexts
+    global squareSide
+    global distanceBetweenSquares
+    global padding
 
     row = len(indices)
     column = len(indices[0])
-    squareSide = 20
-    distanceBetweenSquares = 60
-    padding = 40
+
+
+    
     width = row * (squareSide + distanceBetweenSquares) + padding  * 3
     height = column * (squareSide + distanceBetweenSquares) + padding  * 3
     cree_fenetre(width , height)
-    for j in range(0, column + 1):
-        starty = padding + j * (squareSide + distanceBetweenSquares)
-        endy = padding + j * (squareSide + distanceBetweenSquares) + squareSide
+    for i in range(0, row + 1):
+        starty = padding + i * (squareSide + distanceBetweenSquares)
+        endy = padding + i * (squareSide + distanceBetweenSquares) + squareSide
 
-        for i in range(0, row + 1):
+        for j in range(0, column + 1 ):
             
-            color = "black" if i % 2 == 0 else  "green"
-            startx = padding + i * (squareSide + distanceBetweenSquares)
-            endx = padding + i * (squareSide + distanceBetweenSquares)+ squareSide
+            color = "black" if j % 2 == 0 else  "green"
+            startx = padding + j * (squareSide + distanceBetweenSquares)
+            endx = padding + j * (squareSide + distanceBetweenSquares)+ squareSide
 
             print({"x": {"min": startx, "max": endx}, "y": {"min": starty, "max": endy}})
             rectangle(startx, starty, endx, endy, remplissage=color)
             
-            if i < row and j < column and indices[j][i] != None:
-                lstartx = padding + i * (squareSide + distanceBetweenSquares) + squareSide + distanceBetweenSquares / 2
-                lstarty = padding + j * (squareSide + distanceBetweenSquares) + squareSide + distanceBetweenSquares / 2
-                gameTexts[(j, i)] = texte(lstartx, lstarty,str(indices[j][i]), ancrage='center', taille=14)
+            if i != row and j != column and indices[i][j] != None:
+                lstartx = padding + j * (squareSide + distanceBetweenSquares) + squareSide + distanceBetweenSquares / 2
+                lstarty = padding + i * (squareSide + distanceBetweenSquares) + squareSide + distanceBetweenSquares / 2
+                color = "green" if indices[i][j] == 0 else "black"
+                gameTexts[(i, j)] = texte(lstartx, lstarty,str(indices[i][j]), ancrage='center', taille=14, couleur=color)
     print(gameTexts)
 
     #rectangle(200,100,300,150)
@@ -290,7 +528,7 @@ while True:
             handleClick((abscisse(ev), ordonnee(ev)))
         elif view == "game":
             handleGameClick(tev, (abscisse(ev), ordonnee(ev)))
-        print("Clic gauche au point", (abscisse(ev), ordonnee(ev)))
+        
 
     elif tev == 'Quitte':  # on sort de la boucle
         break
